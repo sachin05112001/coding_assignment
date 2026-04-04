ba626ae9fc25e1ae28949c994b5f1dbfd04a88d6
Sachin Shah <sachin05112001@gmail.com>
2026-04-04 22:51:34 +0530

    code same in lecture

--- lecture04/question_1/001helloworld.cpp
+++ lecture04/question_1/001helloworld.cpp
@@ -1,5 +1,29 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"hello world";
+     cout << "hello world";
+	cout << '\n';
+	cout << 10;
+	cout << endl;
+	cout << 3.14;
+	cout << endl;
+	cout << 'x';
+	cout << endl;
+	cout << true;
+	cout << endl;
+	cout << false;
+	cout << endl;
+	cout << 10 + 20;
+	cout << endl;
+	cout << "10+20";
+
+    cout << "hello world"
+	     << endl
+	     << 10
+	     << endl
+	     << 3.14
+	     << endl
+	     << 'x';
+
+	return 0;
 }
