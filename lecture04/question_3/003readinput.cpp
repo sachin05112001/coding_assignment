e6845cf969c82ff7cbe93296d540015f5756726a
Sachin Shah <sachin05112001@gmail.com>
2026-04-04 23:21:41 +0530

    third question done

---
+++ lecture04/question_3/003readinput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cin >> x;
+    
+    cout << x << endl;
+    char ch;
+    cin >> ch;
+    cout << ch << endl;
+    double d;
+    cin >> d;
+    cout << d << endl;
+    return 0;
+}
