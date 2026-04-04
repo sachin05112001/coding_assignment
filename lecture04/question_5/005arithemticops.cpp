a59d6fe43c3e5bc6c860d1983a451e0437b6804f
Sachin Shah <sachin05112001@gmail.com>
2026-04-04 23:44:19 +0530

    question five completed

---
+++ lecture04/question_5/005arithemticops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 7;
+    int b = 2;
+    cout << a << " + " << b << " = " <<  a + b << endl;
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "a/b = " << a / b << endl;
+    cout << "a%b = " << a % b << endl;
+    cout << "3+5*4 = " << 3+5*4 << endl;
+    cout << "8-4/2 = " << 8-4/2 << endl;
+    cout << "(3+5)*4 = " << (3+5) * 4 << endl;
+    cout << "100/10*10 = " << 100 / 10 * 10 << endl;
+    cout << "5-2+3 = " << 5-2+3 << endl; 
+    return 0;
+}
