167e56a7c9c41fceaec043074b62dd32bd5c5068
Sachin Shah <sachin05112001@gmail.com>
2026-04-04 23:30:55 +0530

    completed four question

---
+++ lecture04/question_4/004assignmentoperators.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    x = 10;
+    cout << x << endl;
+    double d;
+    d = 3.14;
+    cout << d << endl;
+    int y = 20;
+    cout << y << endl;
+    int z = 50 + 60;
+    cout << z << endl;
+    int w = x + y + z;
+    cout << w << endl;
+    cout << INT_MIN << endl;
+    cout <<  INT_MAX << endl;
+    cout << LLONG_MIN << endl;
+    cout << LLONG_MAX << endl;
+
+
+    return 0;
+}
