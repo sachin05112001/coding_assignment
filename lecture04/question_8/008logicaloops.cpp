0d8a257e8f0005aa862d9abb1a6cb9d69ba9b794
Sachin Shah <sachin05112001@gmail.com>
2026-04-05 09:54:09 +0530

    question 8 done

---
+++ lecture04/question_8/008logicaloops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "true && true = " << (true && true) << endl;
+    cout << "true && flase = " << (true && false) << endl;
+    cout << "false && true = " << (false && true) << endl;
+    cout << "flase && flase = " << (false && false) << endl;
+    cout << endl;
+    return 0;
+    cout << "true || true = " << (true || true) << endl;
+    cout << "true || flase = " << (true || false) << endl;
+    cout << "false ||true = " << (false or true) << endl;
+    cout << "flase || flase = " << (false or false) << endl;
+    cout << endl;
+    cout << "!true = " << (!true) << endl;
+    cout << "!flase = " << (!false) << endl;
+    return 0;
+}
