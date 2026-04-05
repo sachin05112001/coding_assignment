b5aa0086b3aac33582d8e955c4d68719f4f91706
Sachin Shah <sachin05112001@gmail.com>
2026-04-05 10:06:53 +0530

    question 10 done

---
+++ lecture04/question_10/0110ifelse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cin >> age;
+    if(age >= 18){
+        cout << "you watch dhurandhar" << endl;
+
+    }else {
+        cout << "you cannot watch dhurandhar" << endl;
+    }
+    cout << "you are outside the if-else block" << endl;
+    return 0;
+}
